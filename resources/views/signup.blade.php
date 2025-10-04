8bc02984e6d09f59ad63ebd372cc6022cc33d0b2
Hasiru Herath <hasiruherath.13@gmail.com>
2025-10-04 10:54:10 +0530

    authentication added

---
+++ resources/views/signup.blade.php
@@ -0,0 +1,25 @@
+@extends('app')
+
+@section('content')
+<h2>Sign Up</h2>
+<form method="POST" action="{{ route('register.submit') }}">
+    @csrf
+    <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" id="name" name="name" required>
+    </div>
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" name="email" required>
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" name="password" required>
+    </div>
+    <div class="form-group">
+        <label for="password_confirmation">Confirm Password</label>
+        <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+    </div>
+    <button type="submit" class="btn btn-primary mt-3">Sign Up</button>
+</form>
+@endsection
